5a9b656016561a327bf30a61e982631538ed40c9
Jesus Antonio <jesus_reddead@hotmail.com>
2022-07-16 14:37:33 -0500

    first commit

---
+++ Sencillo/Uno.java
@@ -0,0 +1,18 @@
+// El nombre del Archivo puede ser diferente a la class mientras las clases no sean publicas
+/*
+ * Por ejemplo
+ * Nombre del archivo pepito
+ * Class Juananitlan
+ * Class Polanco
+*/
+class Uno {
+    
+}
+
+class Dos {
+    
+}
+
+class Tres {
+    
+}
